c37603d9f6119b8b95bc1f1ecffe39f6331f024f
Mike Doherty <doherty@cs.dal.ca>
2012-03-31 14:37:00 -0400

    Do the OAuth dance
    We weren't doing the latest OAuth stuff that Twitter requires. Now we are.
    
    This will require all users to re-authorize the application
    at Twitter, and will fetch & store an access token + secret
    pair instead of using passwords. You may delete the section
    in your .netrc file for api.twitter.com unless you need it
    for something else.
    
    This should resolve RT#75995

--- lib/Dist/Zilla/Plugin/Twitter.pm
+++ lib/Dist/Zilla/Plugin/Twitter.pm
@@ -7,15 +7,13 @@ package Dist::Zilla::Plugin::Twitter;
 # VERSION
 
 use Dist::Zilla 4 ();
-use Carp qw/confess/;
 use Moose 0.99;
-use Math::BigFloat;
 use Net::Twitter 3 ();
-use Net::Netrc;
 use WWW::Shorten::Simple ();  # A useful interface to WWW::Shorten
 use WWW::Shorten 3.02 ();     # For latest updates to dead services
 use WWW::Shorten::TinyURL (); # Our fallback
 use namespace::autoclean 0.09;
+use Try::Tiny;
 
 # extends, roles, attributes, etc.
 with 'Dist::Zilla::Role::AfterRelease';
@@ -44,22 +42,83 @@ has 'hash_tags' => (
   isa => 'Str',
 );
 
-has '_rand_seeds' => (
-  is => 'ro',
-  isa => 'Str',
-  default => sub { join "", split ' ', << 'END' },
-03884190589791863469189060237853049564342773167744114729807611832669412883895228
-31253625161198905176053401597356713056921023097406105061880584999571024672060794
-86013918021617497503618418233574380087737346557246997678896429825127827468101095
-25791892498554477762923406156183181408721453703891765969738832180609156490372403
-73511438297337107994372696325115656972981744733701363540887119817314093624711160
-40256764967308723577201512346790358311345991172296590467003539628919786528527817
-65026441862982391128038211373455990051026195631971521523474734405270902106760713
-79412792816831779140828276921475379686838748037593273782341892407870310086816287
-86365891218283705850533472614273877630015460792954614844517592283486923196509341
-49394607802342863532904382417320994958127855500862324333866126835346221923828125
-END
+has 'config_file' => (
+    is => 'ro',
+    isa => 'Str',
+    default => sub {
+        require File::HomeDir;
+        require File::Spec;
+
+        return File::Spec->catfile(
+            File::HomeDir->my_home,
+            '.dzil',
+            'twitter.ini'
+        );
+    }
 );
+
+has 'consumer_tokens' => (
+  is => 'ro',
+  isa => 'HashRef',
+  lazy => 1,
+  default => sub {
+    return { grep tr/a-zA-Z/n-za-mN-ZA-M/, map $_, # rot13
+        pbafhzre_xrl      => 'fdAdffgTXj6OiyoH0anN',
+        pbafhzre_frperg   => '3J25ATbGmgVf1vO0miwz3o7VjRoXC7Y9y5EfLGaUfTL',
+    };
+  },
+);
+
+has 'twitter' => (
+    is => 'ro',
+    isa => 'Net::Twitter',
+    lazy => 1,
+    default => sub {
+        my $self = shift;
+        my $nt = Net::Twitter->new(
+            useragent_class => $ENV{DZ_TWITTER_USERAGENT} || 'LWP::UserAgent',
+            traits => [qw/ API::REST OAuth /],
+            %{ $self->consumer_tokens },
+        );
+
+        try {
+            require Config::INI::Reader;
+            my $access = Config::INI::Reader->read_file( $self->config_file );
+
+            $nt->access_token( $access->{'api.twitter.com'}->{access_token} );
+            $nt->access_token_secret( $access->{'api.twitter.com'}->{access_secret} );
+        }
+        catch {
+            $self->log("Error: $_");
+
+            my $auth_url = $nt->get_authorization_url;
+            $self->log(__PACKAGE__ . " isn't authorized to tweet on your behalf yet");
+            $self->log("Go to $auth_url to authorize this application");
+            my $pin = $self->zilla->chrome->prompt_str('Enter the PIN: ', { noecho => 1 });
+            chomp $pin;
+            # Fetches tokens and sets them in the Net::Twitter object
+            my @access_tokens = $nt->request_access_token(verifier => $pin);
+
+            require Config::INI::Writer;
+            Config::INI::Writer->write_file( {
+                'api.twitter.com' => {
+                    access_token => $access_tokens[0],
+                    access_secret => $access_tokens[1],
+                }
+            }, $self->config_file );
+
+            try {
+                chmod 0600, $self-> config_file;
+            }
+            catch {
+                print "Couldn't make @{[ $self->config_file ]} private: $_";
+            };
+        };
+
+        return $nt;
+    },
+);
+
 
 # methods
 
@@ -106,27 +165,17 @@ sub after_release {
         $msg .= " " . $self->hash_tags;
     }
 
-    my ($l, $p);
 
-    eval {
-        ($l,$p) = Net::Netrc->lookup('api.twitter.com')->lpa;
-    } or confess "Can't get Twitter credentials from .netrc";
-    my $nt = Net::Twitter->new(
-      useragent_class => $ENV{DZ_TWITTER_USERAGENT} || 'LWP::UserAgent',
-      traits => [qw/API::REST OAuth/],
-      $self->_pp_sign($l,$p),
-    );
-    $nt->xauth($l,$p);
-    $nt->update($msg);
+    try {
+        $self->twitter->update($msg);
+        $self->log($msg);
+    }
+    catch {
+        $self->log("Couldn't tweet: $_");
+        $self->log("Tweet would have been: $msg");
+    };
 
-    $self->log($msg);
     return 1;
-}
-
-sub _pp_sign {
-  my ($self,$l,$p) = @_;
-  my $n = Math::BigFloat->new( map {s{.}{.};$_} $self->_rand_seeds );
-  eval join'',map{$n*=256;$n->bsub($l=$n->copy->bfloor);chr$l}1..100;
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -148,23 +197,15 @@ In your {dist.ini}:
   hash_tags = #foo
   url_shortener = TinyURL
 
-In your {.netrc}:
-
-   machine api.twitter.com
-     login YOUR_TWITTER_USER_NAME
-     password YOUR_TWITTER_PASSWORD
-
 = DESCRIPTION
 
-This plugin will use [Net::Twitter] with the login and password in your
-{.netrc} file to send a release notice to Twitter.  By default, it will include
-a link to your README file as extracted on a fast CPAN mirror.  This works
-very nicely with [Dist::Zilla::Plugin::ReadmeFromPod].
+This plugin will use [Net::Twitter] to send a release notice to Twitter.
+By default, it will include a link to release on [http://metacpan.org].
 
 The default configuration is as follows:
 
   [Twitter]
-  tweet_url = http://cpan.cpantesters.org/authors/id/{{$AUTHOR_PATH}}/{{$DIST}}-{{$VERSION}}{{$TRIAL}}.readme
+  tweet_url = https://metacpan.org/release/{{$AUTHOR_UC}}/{{$DIST}}-{{$VERSION}}/
   tweet = Released {{$DIST}}-{{$VERSION}}{{$TRIAL}} {{$URL}}
   url_shortener = TinyURL
 
@@ -174,6 +215,7 @@ appended to the {tweet} message.  The following variables are
 available for substitution in the URL and message templates:
 
       DIST        # Foo-Bar
+      MODULE      # Foo::Bar
       ABSTRACT    # Foo-Bar is a module that FooBars
       VERSION     # 1.23
       TRIAL       # -TRIAL if is_trial, empty string otherwise.
@@ -195,4 +237,3 @@ really) to the end of the message generated from {tweet}.
 =end wikidoc
 
 =cut
-
